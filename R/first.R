dbe513b54eceda6a1893c40f5afa85757bc7008c
Samantha Tyner <sctyner@iastate.edu>
2014-10-17 14:18:41 -0500

    first files commit

---
+++ R/first.R
@@ -0,0 +1,8 @@
+#' test function
+#' 
+#' this is what the function does
+#' @export
+#' @param x numeric vector
+#' @param ... addiional parameters passed on to min
+#' @return minimum of the vector x
+minx <-  function(x, ...) return(min(x, ...))
