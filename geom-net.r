89c00e01ef4eeb0614e7e2f5132a270bcfe8307a
Samantha Tyner <sctyner@iastate.edu>
2014-11-16 13:18:54 -0600

    Adding to calculate part of stat-net and no real changes to geom-net

--- geom-net.r
+++ geom-net.r
@@ -6,13 +6,13 @@
 #' @export
 #' @inheritParams geom_point
 #' @examples
-#' This is Mortiz Marbach's plotg function, from his post at http://sumtxt.wordpress.com/2011/07/02/visualizing-networks-with-ggplot2-in-r/. 
+#' This is Mortiz Marbach's plotg function, from his post at http://sumtxt.wordpress.com/2011/07/02/visualizing-networks-with-ggplot2-in-r/.
 #' plotg <- function(net, value=NULL) {
 #'  m <- as.matrix.network.adjacency(net) # get sociomatrix
 #'   # get coordinates from Fruchterman and Reingold's force-directed placement algorithm.
-#'  plotcord <- data.frame(gplot.layout.fruchtermanreingold(m, NULL)) 
-#'   # or get it them from Kamada-Kawai's algorithm: 
-#'  plotcord <- data.frame(gplot.layout.kamadakawai(m, NULL)) 
+#'  plotcord <- data.frame(gplot.layout.fruchtermanreingold(m, NULL))
+#'   # or get it them from Kamada-Kawai's algorithm:
+#'  # plotcord <- data.frame(gplot.layout.kamadakawai(m, NULL))
 #'colnames(plotcord) = c("X1","X2")
 #'edglist <- as.matrix.network.edgelist(net)
 #'edges <- data.frame(plotcord[edglist[,1],], plotcord[edglist[,2],])
@@ -20,17 +20,17 @@
 #'colnames(edges) <-  c("X1","Y1","X2","Y2")
 #'edges$midX  <- (edges$X1 + edges$X2) / 2
 #'edges$midY  <- (edges$Y1 + edges$Y2) / 2
-#'pnet <- ggplot()  + 
-#'  geom_segment(aes(x=X1, y=Y1, xend = X2, yend = Y2), 
+#'pnet <- ggplot()  +
+#'  geom_segment(aes(x=X1, y=Y1, xend = X2, yend = Y2),
 #'               data=edges, size = 0.5, colour="grey") +
 #'  geom_point(aes(X1, X2,colour=elements), data=plotcord) +
 #'  scale_colour_brewer(palette="Set1") +
 #'  scale_x_continuous(breaks = NA) + scale_y_continuous(breaks = NA) +
-#'  # discard default grid + titles in ggplot2 
+#'  # discard default grid + titles in ggplot2
 #'  opts(panel.background = theme_blank()) + opts(legend.position="none")+
 #'  opts(axis.title.x = theme_blank(), axis.title.y = theme_blank()) +
-#'  opts( legend.background = theme_rect(colour = NA)) + 
-#'  opts(panel.background = theme_rect(fill = "white", colour = NA)) + 
+#'  opts( legend.background = theme_rect(colour = NA)) +
+#'  opts(panel.background = theme_rect(fill = "white", colour = NA)) +
 #'  opts(panel.grid.minor = theme_blank(), panel.grid.major = theme_blank())
 #' return(print(pnet))
 #' }
@@ -38,7 +38,7 @@
 #'
 #' g <- network(150, directed=FALSE, density=0.03)
 #' classes <- rbinom(150,1,0.5) + rbinom(150,1,0.5) + rbinom(150,1,0.5)
-#' set.vertex.attribute(g, "elements", classes) 
+#' set.vertex.attribute(g, "elements", classes)
 #' plotg(g)
 
   geom_net <- function (mapping = NULL, data = NULL, stat = "net", position = "identity", ...) {
@@ -47,7 +47,7 @@
 
 GeomNet <- proto(Geom, {
   draw <- function(., data, scales, coordinates, ...) {
-    #Something here. 
+    #Something here.
     #
     GeomPoint$draw(data, scales, coordinates, ...)
     GeomSegment$draw(data, scales, coordinates, ...)
@@ -58,7 +58,7 @@ GeomNet <- proto(Geom, {
   guide_geom <- function(.) "point"
 
   default_stat <- function(.) StatNet
-  required_aes <- c("net")
+  required_aes <- c('net')
   default_aes <- function(.) {
     aes(colour = NA, fill = "grey60", size = 0.5, linetype = 1, weight = 1, , alpha = NA, shape = 16)
   }
--- stat-net.r
+++ stat-net.r
@@ -11,8 +11,8 @@
 #'    a warning.  If \code{TRUE} silently removes missing values.
 #' @inheritParams stat_identity
 #' @return A data frame in the same format as \code{\link{stat_contour}}
-#' 
-#@importFrom network 
+#'
+#@importFrom network
 #' @export
 #' @examples
 #' \donttest{
@@ -34,11 +34,38 @@ StatNet <- proto(Stat, {
 
 #calculate function: extracts smaller data frame from original data using just req params. then computes some other stuff.
   calculate <- function(., data, scales, na.rm = FALSE,  layout, ...) {  #add requirements here later.
-  	#compute x and y using layout method if x,y are not there. then can assume that x and y are there from now on. 
+  	#compute x and y using layout method if x,y are not there. then can assume that x and y are there from now on.
+    #library(network,sna) #how/where to call igraph pkg here?
+    browser()
     df <- data.frame(data)
-   
-   browser()
-   #at the end of this routine, we need to have an x and a y.
-   df  
+     #first column of data frame needs to be the 'from' vertex.
+     #second column of data frame needs to be the 'to' vertex. other cols can be anything
+    edges <- data.frame("from" = df[,1], "to" = df[,2])
+    vert.labels <- unique(c(edges$from,edges$to)) #needed here?
+    net <- as.network(edges, matrix.type = "edgelist") #from network package
+    m <- as.matrix.network.adjacency(net)
+    #change the stuff below later to include multiple layouts (in if else statements?)
+    vert.coord <- data.frame(gplot.layout.kamadakawai(m, NULL)) # from sna package
+    vert.coord$name <- sort(vert.labels) #needed here?
+    edgelist <- as.matrix.network.edgelist(net)
+    edge.coord <- data.frame(vert.coord[edgelist[,1],], vert.coord[edgelist[,2],])
+    names(edge.coord) <- c('x','y','xend','yend')
+    #at the end of this routine, we need to have an x and a y. CHECK!
+
+#where to put this?
+#     net.plot <- ggplot()  +
+#       geom_segment(aes(x = x, y = y, xend = xend, yend = yend),
+#                    data = edge.coord, size = , colour="grey") +
+#       geom_point(aes(X1, X2,colour=elements), data=plotcord) +
+#       scale_colour_brewer(palette="Set1") +
+#       scale_x_continuous(breaks = NULL) + scale_y_continuous(breaks = NULL) +
+#       # discard default grid + titles in ggplot2
+#       theme(panel.background = element_blank(), legend.position="none", #changed opts to theme
+#             axis.title.x = element_blank(), axis.title.y = element_blank(), #changed theme_blank to element_blank
+#             legend.background = element_rect(colour = NA),
+#             panel.background = element_rect(fill = "white", colour = NA),
+#             panel.grid.minor = element_blank(), panel.grid.major = element_blank())
+#
+
   }
 })
