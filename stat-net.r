e1c8cdb6703b28f1a32f7cd22fabefbcb6ca9f5c
Samantha Tyner <sctyner@iastate.edu>
2015-04-02 12:02:12 -0500

    update headers

--- stat-net.r
+++ stat-net.r
@@ -1,23 +1,7 @@
 #' 2d density estimation.
-#'
-#' @section Aesthetics:
-#' \Sexpr[results=rd,stage=build]{ggplot2:::rd_aesthetics("stat", "density2d")}
-#'
-#' @param contour If \code{TRUE}, contour the results of the 2d density
-#'   estimation
-#' @param n number of grid points in each direction
-#' @param ... other arguments passed on to \code{\link{kde2d}}
-#' @param na.rm If \code{FALSE} (the default), removes missing values with
-#'    a warning.  If \code{TRUE} silently removes missing values.
-#' @inheritParams stat_identity
-#' @return A data frame in the same format as \code{\link{stat_contour}}
-#'
-#@importFrom network
-#' @export
-#' @examples
-#' \donttest{
-#' #put in example here.
-#' }
+#' #Documentation to be added
+#' 
+#' 
 stat_net <- function (mapping = NULL, data = NULL, vertices = NULL,  geom = "net", position = "identity", ...) {
 #  print("new net stat")
 
