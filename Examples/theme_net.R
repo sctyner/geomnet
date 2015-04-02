e7c5cfb526b2a9c328df57b2b55e0802de125b3e
Samantha Tyner <sctyner@iastate.edu>
2015-04-02 15:09:57 -0500

    add theme object to examples

---
+++ Examples/theme_net.R
@@ -0,0 +1,8 @@
+# theme_net. used in other examples.
+
+theme_net <- theme(aspect.ratio = 1, 
+                   line = element_blank(), 
+                   panel.background = element_blank(), 
+                   plot.background = element_blank(), 
+                   axis.text=element_blank(),
+                   axis.title=element_blank())
